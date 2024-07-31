3b274a3d02b8e39da10e387f0dc100868b755880
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-07-31 01:07:20 -0300

    Switching letters

---
+++ ponteiro.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+char * troca(char *str, char oldLetter,char newLetter);
+int main(int argc, char *argv[])
+{
+    char str[100];
+    char newLetter, oldLetter;
+
+    printf("Escreva uma string: ");
+    fgets(str, 100, stdin);
+
+
+    printf("Qual letra sera trocada: ");
+    oldLetter = getchar();
+    getchar();
+    printf("Qual letra substituira: \n");
+    newLetter = getchar();
+
+    printf("A string escrita foi: %s\n", str);    printf("A string escrita tornou-se: %s\n", troca(str, oldLetter, newLetter));
+
+    return 0;
+}
+
+char * troca(char *str, char oldLetter, char newLetter)
+{
+    char *inicio = str;
+
+    while(*str != '\0')
+    {
+        if(*str == oldLetter)
+        *str = newLetter;
+        str++;
+    }
+    return inicio;
+}
